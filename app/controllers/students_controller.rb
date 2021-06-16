9e14ad6898ea1a9e239e754586455f015f304d79
Xiaochen Xu <xiaochex@gmail.com>
2021-06-16 18:26:51 -0400

    commit tree df30dfb1169d57ffd1c2d9c65bc9350a6d1eacc9

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    #always remember if use "find_by", don't forget to add id: "then params"
+    render json: student
+    # byebug
+  end
 end
